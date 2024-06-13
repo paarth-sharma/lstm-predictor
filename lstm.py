fc188da8a5a5d0da60e7a231e1f64a1f111372ab
paarth-sharma <paarthsharma2003@gmail.com>
2024-06-13 16:48:06 +0530

    removed the redundant part of request

--- lstm.py
+++ lstm.py
@@ -1,22 +1,4 @@
 import requests
-
-url = "https://api.upstox.com/v2/historical-candle/NSE_EQ%7CINE848E01016/1minute/2024-06-12/2014-06-12"
-
-headers = {
-  'Accept': 'application/json'
-}
-
-response = requests.get(url, headers=headers)
-
-# Check the response status
-if response.status_code == 200:
-    # Do something with the response data (e.g., print it)
-    print(response.json())
-else:
-    # Print an error message if the request was not successful
-    print(f"Error: {response.status_code} - {response.text}")
-
-import requests
 import json
 from datetime import datetime, timedelta
 
